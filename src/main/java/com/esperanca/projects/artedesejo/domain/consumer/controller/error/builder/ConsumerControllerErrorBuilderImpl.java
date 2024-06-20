d8777204de9a1638324c852ed5f3c326bd241bae
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-20 17:51:46 -0300

    Implement ConsumerControllerErrorBuilderImpl for building consumer controller errors

---
+++ src/main/java/com/esperanca/projects/artedesejo/domain/consumer/controller/error/builder/ConsumerControllerErrorBuilderImpl.java
@@ -0,0 +1,12 @@
+package com.esperanca.projects.artedesejo.domain.consumer.controller.error.builder;
+
+import com.esperanca.projects.artedesejo.core.controller.error.builder.ControllerErrorBuilder;
+import com.esperanca.projects.artedesejo.domain.consumer.controller.error.enums.ConsumerControllerErrorType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerControllerErrorBuilderImpl
+    implements ControllerErrorBuilder<ConsumerControllerErrorType>
+{
+
+}
