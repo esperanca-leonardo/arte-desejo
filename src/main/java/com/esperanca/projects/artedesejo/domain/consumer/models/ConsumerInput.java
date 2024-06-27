28bcd55b576c8c3ea35f7073c7361a51e78f9428
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-27 17:18:30 -0300

    Added some constraint validations

--- src/main/java/com/esperanca/projects/artedesejo/domain/consumer/models/ConsumerInput.java
+++ src/main/java/com/esperanca/projects/artedesejo/domain/consumer/models/ConsumerInput.java
@@ -3,19 +3,27 @@ package com.esperanca.projects.artedesejo.domain.consumer.models;
 import com.esperanca.projects.artedesejo.domain.address.models.AddressInput;
 import com.esperanca.projects.artedesejo.domain.consumer.enums.Gender;
 import com.esperanca.projects.artedesejo.domain.consumer.enums.SexualOrientation;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
 @Getter
 public class ConsumerInput
 {
+  @NotBlank
   private String name;
   private Gender gender;
   private SexualOrientation sexualOrientation;
   private LocalDate dateOfBirth;
   private String phoneNumber;
+
+  @Email
   private String email;
+
+  @CPF
   private String cpf;
   private AddressInput address;
 }
--- src/main/java/com/esperanca/projects/artedesejo/domain/supplier/models/SupplierInput.java
+++ src/main/java/com/esperanca/projects/artedesejo/domain/supplier/models/SupplierInput.java
@@ -1,9 +1,11 @@
 package com.esperanca.projects.artedesejo.domain.supplier.models;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class SupplierInput
 {
+  @NotBlank
   private String name;
 }
