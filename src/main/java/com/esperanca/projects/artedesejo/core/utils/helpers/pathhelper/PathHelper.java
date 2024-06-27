df69d98606547e172a580485e419eb2427908ba6
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-27 15:02:57 -0300

    Add PathHelper interface and its implementation for generating entity paths

---
+++ src/main/java/com/esperanca/projects/artedesejo/core/utils/helpers/pathhelper/PathHelper.java
@@ -0,0 +1,6 @@
+package com.esperanca.projects.artedesejo.core.utils.helpers.pathhelper;
+
+public interface PathHelper
+{
+  String getEntityPath(String className);
+}
---
+++ src/main/java/com/esperanca/projects/artedesejo/core/utils/helpers/pathhelper/PathHelperImpl.java
@@ -0,0 +1,20 @@
+package com.esperanca.projects.artedesejo.core.utils.helpers.pathhelper;
+
+import org.springframework.stereotype.Component;
+
+import static java.lang.String.format;
+
+@Component
+public class PathHelperImpl implements PathHelper
+{
+  public static final String DOMAIN_PACKAGE = "com.esperanca.projects" +
+      ".artedesejo.domain";
+
+  @Override
+  public String getEntityPath(String className)
+  {
+    return format("%s.%s.entity.%s",
+        DOMAIN_PACKAGE, className.toLowerCase(), className
+    );
+  }
+}
