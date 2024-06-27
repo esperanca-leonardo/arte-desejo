db8ac69a0fcdb7640f7a232a565c22ab50c0088b
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-27 17:09:24 -0300

    Add SupplierProblemDetailBuilder implementation

---
+++ src/main/java/com/esperanca/projects/artedesejo/domain/supplier/controller/error/builder/SupplierProblemDetailBuilderImpl.java
@@ -0,0 +1,12 @@
+package com.esperanca.projects.artedesejo.domain.supplier.controller.error.builder;
+
+import com.esperanca.projects.artedesejo.core.error.problemdetail.contracts.ProblemDetailBuilder;
+import com.esperanca.projects.artedesejo.domain.supplier.controller.error.enums.SupplierProblemDetailType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SupplierProblemDetailBuilderImpl
+    implements ProblemDetailBuilder<SupplierProblemDetailType>
+{
+
+}
