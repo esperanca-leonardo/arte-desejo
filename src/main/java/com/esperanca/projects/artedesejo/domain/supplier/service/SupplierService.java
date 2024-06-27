73018c670bd6da82e2cdf0eaec9f6bb5c3e72054
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-27 15:10:59 -0300

    Implement SupplierService to retrieve suppliers by ID

--- src/main/java/com/esperanca/projects/artedesejo/domain/supplier/service/SupplierService.java
+++ src/main/java/com/esperanca/projects/artedesejo/domain/supplier/service/SupplierService.java
@@ -1,27 +1,9 @@
 package com.esperanca.projects.artedesejo.domain.supplier.service;
 
-import com.esperanca.projects.artedesejo.core.contracts.crud.Crud;
+import com.esperanca.projects.artedesejo.domain.supplier.entity.Supplier;
 import com.esperanca.projects.artedesejo.domain.supplier.exceptions.SupplierNotFoundException;
-import com.esperanca.projects.artedesejo.domain.supplier.models.SupplierInput;
-import com.esperanca.projects.artedesejo.domain.supplier.models.SupplierOutput;
 
-import java.util.List;
-
-public interface SupplierService extends Crud<SupplierOutput, SupplierInput, Long>
+public interface SupplierService
 {
-  @Override
-  List<SupplierOutput> findAll();
-
-  @Override
-  SupplierOutput findById(Long id) throws SupplierNotFoundException;
-
-  @Override
-  SupplierOutput save(SupplierInput supplierInput);
-
-  @Override
-  SupplierOutput updateById(Long id, SupplierInput supplierInput)
-      throws SupplierNotFoundException;
-
-  @Override
-  void deleteById(Long id);
+  Supplier findById(Long id) throws SupplierNotFoundException;
 }
