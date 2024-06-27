fc74c4796e6c64693fcdb4da6e0e3745938e2c89
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-27 17:13:41 -0300

    ConsumerProblemDetailBuilder implementation

---
+++ src/main/java/com/esperanca/projects/artedesejo/domain/consumer/controller/error/builder/ConsumerProblemDetailBuilderImpl.java
@@ -0,0 +1,12 @@
+package com.esperanca.projects.artedesejo.domain.consumer.controller.error.builder;
+
+import com.esperanca.projects.artedesejo.core.error.problemdetail.contracts.ProblemDetailBuilder;
+import com.esperanca.projects.artedesejo.domain.consumer.controller.error.enums.ConsumerProblemDetailType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerProblemDetailBuilderImpl
+    implements ProblemDetailBuilder<ConsumerProblemDetailType>
+{
+
+}
