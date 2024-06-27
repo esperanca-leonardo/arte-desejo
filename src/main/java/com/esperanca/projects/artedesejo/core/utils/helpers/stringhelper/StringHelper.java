252bf5f4a0f8678e22976be7e0caaeed2436c27c
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-27 15:02:11 -0300

    Add StringHelper interface and its implementation for removing suffixes

---
+++ src/main/java/com/esperanca/projects/artedesejo/core/utils/helpers/stringhelper/StringHelper.java
@@ -0,0 +1,6 @@
+package com.esperanca.projects.artedesejo.core.utils.helpers.stringhelper;
+
+public interface StringHelper
+{
+  String removeSuffix(String className, String suffix);
+}
---
+++ src/main/java/com/esperanca/projects/artedesejo/core/utils/helpers/stringhelper/StringHelperImpl.java
@@ -0,0 +1,19 @@
+package com.esperanca.projects.artedesejo.core.utils.helpers.stringhelper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringHelperImpl implements StringHelper
+{
+  @Override
+  public String removeSuffix(String className, String suffix)
+  {
+    if (!className.endsWith(suffix))
+    {
+      throw new IllegalArgumentException("The suffix is not present");
+    }
+
+    final int lengthWithoutSuffix = className.length() - suffix.length();
+    return className.substring(0, lengthWithoutSuffix);
+  }
+}
