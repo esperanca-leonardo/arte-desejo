663d06d09755626c406976132d911a28a0039f5d
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-06-28 19:00:43 -0300

    Updated V2__Create_Consumer_Table.sql to include validation checks for gender and sexual_orientation fields with specific allowed values.

--- src/main/resources/db/migration/V2__Create_Consumer_Table.sql
+++ src/main/resources/db/migration/V2__Create_Consumer_Table.sql
@@ -2,8 +2,22 @@ CREATE TABLE consumer (
     id SERIAL PRIMARY KEY,
 
     name VARCHAR NOT NULL,
-    gender VARCHAR,
-    sexual_orientation VARCHAR,
+    gender VARCHAR CHECK (
+        gender IN (
+            'MAN', 'CIS_MAN', 'INTERSEX_MAN', 'TRANS_MAN', 'TRANSMASCULINE',
+            'WOMAN', 'CIS_WOMAN', 'INTERSEX_WOMAN', 'TRANS_WOMAN',
+            'TRANS_FEMININE', 'BEYONDE_BINARY', 'AGENDER', 'BIGENDER',
+            'GENDERFLUID', 'GENDER_QUESTIONING', 'GENDERQUEER', 'INTERSEX',
+            'NONBINARY', 'PANGENDER', 'TRANS_PERSON', 'TWO_SPIRIT', 'NOT_LISTED'
+        )
+    ),
+    sexual_orientation VARCHAR CHECK (
+        sexual_orientation IN (
+            'STRAIGHT', 'GAY', 'LESBIAN', 'BISEXUAL', 'ASEXUAL', 'DEMISEXUAL',
+            'PANSEXUAL', 'QUEER', 'QUESTIONING', 'AROMANTIC', 'OMINISEXUAL',
+            'NOT_LISTED'
+        )
+    ),
     date_of_birth DATE,
     phone_number VARCHAR UNIQUE NOT NULL,
     email VARCHAR UNIQUE NOT NULL,
