8cb2739bd934492973060d8f1844278a7973b5b7
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-07-11 12:53:11 -0300

    Created SaleOrderInput

---
+++ src/main/java/com/esperanca/projects/artedesejo/domain/saleorder/models/SaleOrderInput.java
@@ -0,0 +1,23 @@
+package com.esperanca.projects.artedesejo.domain.saleorder.models;
+
+import com.esperanca.projects.artedesejo.core.annotations.Exists;
+import com.esperanca.projects.artedesejo.domain.consumer.models.ConsumerIdInput;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class SaleOrderInput
+{
+  @Valid
+  @Exists
+  @NotNull
+  private ConsumerIdInput consumerId;
+
+  @NotNull
+  @PositiveOrZero
+  private BigDecimal total;
+}
