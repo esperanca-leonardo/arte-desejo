9936488ef1f33f2df357a775c2e5254300a86cc8
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-07-11 09:51:37 -0300

    Extends BaseService

--- src/main/java/com/esperanca/projects/artedesejo/domain/consumer/service/ConsumerService.java
+++ src/main/java/com/esperanca/projects/artedesejo/domain/consumer/service/ConsumerService.java
@@ -1,9 +1,11 @@
 package com.esperanca.projects.artedesejo.domain.consumer.service;
 
+import com.esperanca.projects.artedesejo.core.contracts.base.BaseService;
 import com.esperanca.projects.artedesejo.domain.consumer.entity.Consumer;
 import com.esperanca.projects.artedesejo.domain.consumer.exceptions.crud.ConsumerNotFoundException;
 
-public interface ConsumerService
+public interface ConsumerService extends BaseService<Consumer>
 {
+  @Override
   Consumer findById(Long id) throws ConsumerNotFoundException;
 }
--- src/main/java/com/esperanca/projects/artedesejo/domain/supplier/service/SupplierService.java
+++ src/main/java/com/esperanca/projects/artedesejo/domain/supplier/service/SupplierService.java
@@ -1,9 +1,11 @@
 package com.esperanca.projects.artedesejo.domain.supplier.service;
 
+import com.esperanca.projects.artedesejo.core.contracts.base.BaseService;
 import com.esperanca.projects.artedesejo.domain.supplier.entity.Supplier;
 import com.esperanca.projects.artedesejo.domain.supplier.exceptions.SupplierNotFoundException;
 
-public interface SupplierService
+public interface SupplierService extends BaseService<Supplier>
 {
+  @Override
   Supplier findById(Long id) throws SupplierNotFoundException;
 }
