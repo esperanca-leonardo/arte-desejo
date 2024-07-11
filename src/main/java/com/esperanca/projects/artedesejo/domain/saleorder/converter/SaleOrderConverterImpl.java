a7031ea1cd13ff1957e7247da99a2b2b9fa8a8f5
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-07-11 12:58:28 -0300

    Created SaleOrderConverterImpl

---
+++ src/main/java/com/esperanca/projects/artedesejo/domain/saleorder/converter/SaleOrderConverterImpl.java
@@ -0,0 +1,39 @@
+package com.esperanca.projects.artedesejo.domain.saleorder.converter;
+
+import com.esperanca.projects.artedesejo.core.contracts.helpers.converter.ConverterHelper;
+import com.esperanca.projects.artedesejo.domain.saleorder.entity.SaleOrder;
+import com.esperanca.projects.artedesejo.domain.saleorder.models.SaleOrderInput;
+import com.esperanca.projects.artedesejo.domain.saleorder.models.SaleOrderOutput;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class SaleOrderConverterImpl implements ConverterHelper<SaleOrder,
+    SaleOrderInput, SaleOrderOutput>
+{
+  private final ModelMapper modelMapper;
+
+  @Override
+  public SaleOrder toEntity(SaleOrderInput saleOrderInput)
+  {
+    return this.modelMapper.map(saleOrderInput, SaleOrder.class);
+  }
+
+  @Override
+  public SaleOrderOutput toOutput(SaleOrder entity)
+  {
+    return this.modelMapper.map(entity, SaleOrderOutput.class);
+  }
+
+  @Override
+  public List<SaleOrderOutput> toCollectionOutput(List<SaleOrder> saleOrders)
+  {
+    return saleOrders.stream()
+        .map(this::toOutput)
+        .toList();
+  }
+}
