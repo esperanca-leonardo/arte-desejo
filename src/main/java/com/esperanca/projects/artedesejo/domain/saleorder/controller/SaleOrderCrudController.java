db23444d47dfc367404021def43266e2d7e2b8a2
esperanca-leonardo <dev.esperanca.leonardo@gmail.com>
2024-07-12 15:53:57 -0300

    Added @ResponseStatus annotation

--- src/main/java/com/esperanca/projects/artedesejo/domain/saleorder/controller/SaleOrderCrudController.java
+++ src/main/java/com/esperanca/projects/artedesejo/domain/saleorder/controller/SaleOrderCrudController.java
@@ -5,9 +5,12 @@ import com.esperanca.projects.artedesejo.domain.saleorder.models.SaleOrderOutput
 import com.esperanca.projects.artedesejo.domain.saleorder.service.crud.SaleOrderCrudService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/sale-orders")
@@ -17,24 +20,28 @@ public class SaleOrderCrudController
   private final SaleOrderCrudService service;
 
   @GetMapping
+  @ResponseStatus(OK)
   public List<SaleOrderOutput> findAll()
   {
     return this.service.findAll();
   }
 
   @GetMapping("/{id}")
+  @ResponseStatus(OK)
   public SaleOrderOutput findById(@PathVariable Long id)
   {
     return this.service.findById(id);
   }
 
   @PostMapping
+  @ResponseStatus(CREATED)
   public SaleOrderOutput save(@Valid @RequestBody SaleOrderInput saleOrderInput)
   {
     return this.service.save(saleOrderInput);
   }
 
   @PutMapping("/{id}")
+  @ResponseStatus(OK)
   public SaleOrderOutput updateById(@PathVariable Long id,
                                     @Valid @RequestBody SaleOrderInput saleOrderInput)
   {
@@ -42,6 +49,7 @@ public class SaleOrderCrudController
   }
 
   @DeleteMapping("/{id}")
+  @ResponseStatus(NO_CONTENT)
   public void deleteById(@PathVariable Long id)
   {
     this.service.deleteById(id);
